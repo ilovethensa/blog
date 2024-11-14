0775a40b3f83aedf080f0216811460f115a5768a
ilovethensa <tht@pwned.page>
2024-11-14 14:04:38 +0200

    using yarn again

--- flake.nix
+++ flake.nix
@@ -7,25 +7,11 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-        packages.website = pkgs.buildNpmPackage rec {
-  pname = "flood";
-  version = "4.7.0";
-
+        packages.website = pkgs.mkYarnPackage {
+  name = "hello-purescript";
   src = ./.;
-
-  npmDepsHash = "sha256-tuEfyePwlOy2/mOPdXbqJskO6IowvAP4DWg8xSZwbJw=";
-
-  # The prepack script runs the build script, which we'd rather do in the build phase.
-  npmPackFlags = [ "--ignore-scripts" ];
-
-  NODE_OPTIONS = "--openssl-legacy-provider";
-
-  meta = {
-    description = "Modern web UI for various torrent clients with a Node.js backend and React frontend";
-    homepage = "https://flood.js.org";
-    license = pkgs.lib.licenses.gpl3Plus;
-    maintainers = with pkgs.lib.maintainers; [ winter ];
-  };
+  packageJson = ./package.json;
+  yarnLock = ./yarn.lock;
 };
 
         defaultPackage = self.packages.${system}.website;
