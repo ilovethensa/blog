4489768d6722c4dac0d5bff75c2f91784ed26cfa
ilovethensa <tht@pwned.page>
2024-11-14 14:21:08 +0200

    it finally works

--- flake.nix
+++ flake.nix
@@ -3,18 +3,31 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages.website = pkgs.mkYarnPackage {
-  name = "hello-purescript";
-  src = ./.;
-  packageJson = ./package.json;
-  yarnLock = ./yarn.lock;
-};
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.website = pkgs.mkYarnPackage {
+        name = "blog";
+        src = ./.;
+        packageJson = ./package.json;
+        yarnLock = ./yarn.lock;
+        dontFixup = true;
+        doDist = false;
+        doBuild = false;
+        buildPhase = ''
+          cd deps/lowkey && ${pkgs.hugo}/bin/hugo --minify --gc
+        '';
+        installPhase = ''
+          mv public $out
+        '';
+      };
 
-        defaultPackage = self.packages.${system}.website;
-        devShells.default = pkgs.mkShell { packages = [ pkgs.hugo ]; };
-      });
+      defaultPackage = self.packages.${system}.website;
+      devShells.default = pkgs.mkShell {packages = [pkgs.hugo pkgs.yarn pkgs.nodejs];};
+    });
 }
