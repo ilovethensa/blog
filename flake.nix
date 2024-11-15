1b042bb49cdaa847cf16843fcd9818ece1d2fff4
ilovethensa <tht@pwned.page>
2024-11-15 13:52:49 +0200

    updated donation page

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     in {
       packages.website = pkgs.mkYarnPackage {
         name = "blog";
-        version = "0.1.1";
+        version = "0.1.2";
         src = ./.;
         packageJson = ./package.json;
         yarnLock = ./yarn.lock;
